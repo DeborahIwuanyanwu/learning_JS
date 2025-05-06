1b383bd92a678e5839398b7e0363aff01b3b9384
DeborahIwuanyanwu <deborahiwuanyanwu@gmail.com>
2025-05-06 16:58:21 +0100

    first assignment

---
+++ assignment/index.js
@@ -0,0 +1,13 @@
+prompt("What is your name?")
+prompt ('How old are you?')
+
+const age = 22
+
+console.log('Hello! Agbo Sixtus')
+console.log("You will be", (age + 4), "in the next 4 years")
+
+
+const numberOfPupils = 26;
+const nameOfSchool = 'Prince Charles International School,';
+
+console.log("The name of my school is", nameOfSchool, 'and we are only', numberOfPupils, 'pupils in my class')
